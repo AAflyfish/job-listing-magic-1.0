3bc0162a5b35c13daf78067202e41d7837af80be
afish <owengoal2008@163.com>
2017-05-01 16:00:14 +0800

    better welcome/index $ admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  root 'jobs#index'
+
 
   namespace :admin do
     resources :jobs do
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
